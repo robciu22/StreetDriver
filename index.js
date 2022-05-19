a1ef1422207116241fb2d349944831368314574d
RoLegs <rolegs@etik.com>
2022-05-19 12:46:20 +0200

    StreetDriver

--- index.js
+++ index.js
@@ -2,36 +2,38 @@ let canvas = document.querySelector('canvas')
 let ctx = canvas.getContext('2d')
 canvas.style.border = '2px solid black'
 canvas.style.background = 'blue'
+let gameOverPage = document.querySelector('#gameOverPage')
+
 
 
 let startScreen = document.querySelector('#startScreen')
 
 let roadImage = new Image();
-roadImage.src = "/images/streets/multilaneHighway.jpg"
+roadImage.src = "./images/streets/multilaneHighway.jpg"
 
 let carImage = new Image()
-carImage.src = "images/cars/myCar.jpg"
+carImage.src = "./images/cars/myCar.png"
 
 let carObstOne = new Image()
-carObstOne.src = "images/cars/silverCar.jpeg"
+carObstOne.src = "./images/cars/silverCar.png"
 
 let carObstTwo = new Image()
-carObstTwo.src = "images/cars/greyCar.jpeg"
+carObstTwo.src = "./images/cars/greyCar.png"
 
 let carObstThree = new Image()
-carObstThree.src = "images/cars/yellowCar.jpg"
+carObstThree.src = "./images/cars/yellowCar.png"
 
 let carObstFour = new Image()
-carObstFour.src = "images/cars/cabrioCar.jpg"
+carObstFour.src = "./images/cars/cabrioCar.png"
 
 let carObstCrashed = new Image()
-carObstCrashed.src = "images/cars/crashedCars.jpg"
+carObstCrashed.src = "./images/cars/crashedCars.png"
 
 let obstCarArr = [
     { img: carObstOne, x: Math.floor(Math.random() * (canvas.width - 100)), y: 0 },
-    { img: carObstTwo, x: Math.floor(Math.random() * (canvas.width - 150)), y: 100 },
-    { img: carObstThree, x: Math.floor(Math.random() * (canvas.width - 275)), y: 150 },
-    { img: carObstFour, x: Math.floor(Math.random() * (canvas.width - 400)), y: 200 },
+    { img: carObstTwo, x: Math.floor(Math.random() * (canvas.width - 150)), y: -200 },
+    { img: carObstThree, x: Math.floor(Math.random() * (canvas.width - 275)), y: -650 },
+    { img: carObstFour, x: Math.floor(Math.random() * (canvas.width - 400)), y: -1200 },
     //{ img: carObstCrashed, x: Math.floor(Math.random() * canvas.width), y: 0 },
 ];
 
@@ -40,7 +42,7 @@ const carHeight = 125
 
 let carX = 240 //X-Startposition from myCar
 let carY = 450 //Y-Startposition from myCar
-
+let myCarPos = (carX, carY)
 
 
 let carSpeedValue = 3
@@ -65,7 +67,7 @@ let audio = new Audio("");
 audio.volume = 0.1
 let sound = new Audio("");
 sound.volume = 0.2
-let gameOverSong = new Audio("/audio/mixkit-game-over-trombone-1940.wav");
+let gameOverSong = new Audio("./audio/mixkit-game-over-trombone-1940.wav");
 gameOverSong.volume = 0.1
 
 //  Buttons
@@ -75,23 +77,11 @@ let soundBtn = document.querySelector("#soundBtn");
 let muteBtn = document.querySelector("#muteBtn");
 
 
-
-
-/*
-window.onload = () => {
-    canvas.style.display = 'none'
-    document.getElementById('startBtn').onclick = () => {
-        //the next line is for proofing if connected and StartButton is working...
-        console.log("Startbutton was clicked")
-
-        startGame();
-    };
-*/
+//Display myCar and moving with arrowKeys
 function drawMyCar() {
-    ctx.beginPath()
+
     ctx.drawImage(carImage, carX, carY, carWidth, carHeight)
-    ctx.closePath()
-        //carX = carX - 1
+
     if (isCarGoingLeft) {
         if (carX > 0) {
             carX -= carSpeedValue;
@@ -113,43 +103,31 @@ function drawMyCar() {
 
 //obstacle cars ;-)
 function drawSilverCar() {
-    ctx.beginPath()
     ctx.drawImage(carObstOne, 0, 0, 70, 125)
-    ctx.closePath()
 }
 
 function drawGrayCar() {
-    ctx.beginPath()
     ctx.drawImage(carObstTwo, 0, 200, 70, 125)
-    ctx.closePath()
 }
 
 function drawYellowCar() {
-    ctx.beginPath()
     ctx.drawImage(carObstThree, 200, 0, 70, 125)
-    ctx.closePath()
 }
 
 function drawCabrioCar() {
-    ctx.beginPath()
     ctx.drawImage(carObstFour, 200, 200, 70, 125)
-    ctx.closePath()
 }
 
 function drawCrashedCar() {
-    ctx.beginPath()
     ctx.drawImage(carObstCrashed, 400, 0, 125, 100)
-    ctx.closePath()
 }
 
 
 //Scoreboard
 function drawScore() {
-    ctx.beginPath()
     ctx.font = "30px sans-serif"
     ctx.fillStyle = "white"
-    ctx.fillText(`Score : ${score}`, 80, 50)
-    ctx.closePath()
+    ctx.fillText(`Your Score : ${score}`, 80, 50)
 }
 
 
@@ -168,83 +146,28 @@ function moving() {
         //collision inside of for loop
         if (
             // checks if the bottom of the traffic car is touching the top of the player car
-            currentCar.y + currentCar.height >= carY + 10 &&
+            currentCar.y + 125 >= carY + 10 &&
             //checks if the right side of the player car is more to the right than the traffic car
             carX + 120 > currentCar.x &&
             // checks if the left side of the player car is touching the left side of the traffic car
-            carX < currentCar.x + currentCar.width &&
+            carX < currentCar.x + 70 &&
             //checks if the bottom of the player car is touching the top of the traffic car
             carY + carHeight - 10 > currentCar.y
         ) {
+            console.log('working')
             isGameOver = true;
         }
 
     }
-
-    //Car-Collision
-    // function carCollision(myCar, currentCar) {
-    //     if (myCar.x + myCar.width >= currentCar.x &&
-    //         myCar.x <= currentCar.x + currentCar.width &&
-    //         myCar.y + myCar.height >= currentCar.y &&
-    //         myCar.y <= currentCar.y + currentCar.height
-    //     )
-    //         return true
-    // }
-
-    // if (carCollision != 'true') {
-    //     isGameOver = true
-    // }
-
-
-
-    // function isColloide(a,b){
-    //     return !(
-    //         ((a.y + a.height) < (b.y)) ||
-    //         (a.y > (b.y + b.height)) ||
-    //         ((a.x + a.width) < b.x) ||
-    //         (a.x > (b.x + b.width))
-    //     )
-
-    // }
-
-    // let measureMyCar = { rect: 20, x: 75, y: 125 }
-    // let measureObstCar = { rect: 20, x: 75, y: 125 }
-
-    // let dim1 = measureMyCar.x - measureObstCar.x
-    // let dim2 = measureMyCar.y - measureObstCar.y
-
-    // let distance = Math.sqrt(dim1 * dim1 + dim2 * dim2);
-
-    // if (distance < measureMyCar.rect + measureObstCar.rect) {
-    //     isGameOver = true;
-    // }
-
 }
 
 
-
-
 function startGame() {
-    ctx.beginPath()
+
     ctx.drawImage(roadImage, 0, 0, canvas.width, canvas.height)
-    ctx.closePath()
 
     drawMyCar()
 
-    // for (let i = 0; i < obstCarArr.length; i += 1) {
-    //     let currentCar = obstCarArr[i];
-
-    //     drawSilverCar()
-    // }
-    // drawSilverCar()
-
-    // drawGrayCar()
-
-    // drawYellowCar()
-
-    // drawCabrioCar()
-
-    // drawCrashedCar()
 
     moving()
 
@@ -258,6 +181,8 @@ function startGame() {
         score = 0
         carSpeedValue = 3
         gameOverSong.play()
+        canvas.style.display = 'none'
+        gameOverPage.style.display = 'block'
     } else {
         intervalId = requestAnimationFrame(startGame)
     }
@@ -267,19 +192,38 @@ function startGame() {
 
 window.addEventListener('load', () => {
     canvas.style.display = 'none'
+    gameOverPage.style.display = 'none'
     startBtn.addEventListener('click', () => {
-        console.log('click')
         startScreen.style.display = 'none'
         canvas.style.display = 'block'
 
+        myCarPos //X- und Y-Startposition from myCar
+
         startGame()
+        console.log('start works')
     })
     restartBtn.addEventListener('click', () => {
         console.log('click')
         startScreen.style.display = 'none'
         canvas.style.display = 'block'
+        gameOverPage.style.display = 'none'
+        isGameOver = false
+
+        obstCarArr = [
+            { img: carObstOne, x: Math.floor(Math.random() * (canvas.width - 100)), y: 0 },
+            { img: carObstTwo, x: Math.floor(Math.random() * (canvas.width - 200)), y: -300 },
+            { img: carObstThree, x: Math.floor(Math.random() * (canvas.width - 300)), y: -650 },
+            { img: carObstFour, x: Math.floor(Math.random() * (canvas.width - 400)), y: -1200 },
+            { img: carObstOne, x: Math.floor(Math.random() * (canvas.width - 250)), y: 0 },
+            { img: carObstTwo, x: Math.floor(Math.random() * (canvas.width - 300)), y: -400 },
+            { img: carObstThree, x: Math.floor(Math.random() * (canvas.width - 450)), y: -850 },
+            { img: carObstFour, x: Math.floor(Math.random() * (canvas.width - 500)), y: -1500 },
+        ];
+
+        myCarPos //X- und Y-Startposition from myCar
 
         startGame()
+        console.log('restart working')
     })
 })
 
